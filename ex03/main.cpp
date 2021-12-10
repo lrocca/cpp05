b3af663d6877c1dea1e429c9a70b02e6d7046d22
Leonardo Rocca <lrocca@student.42roma.it>
2021-12-10 20:17:27 +0100

    ex03: tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,49 +6,42 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 17:35:33 by lrocca            #+#    #+#             */
-/*   Updated: 2021/12/08 02:59:49 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/12/10 20:13:00 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "Intern.hpp"
 
-// void	sectionTitle(const std::string& title) {
-// 	std::cout	<< std::endl
-// 				<< "*** " << title << " ***"
-// 				<< std::endl << std::endl;
-// }
+void	sectionTitle(const std::string& title) {
+	std::cout	<< std::endl
+				<< "*** " << title << " ***"
+				<< std::endl << std::endl;
+}
+
+Bureaucrat	burro("burro", 42);
+Intern		interno;
+
+void	testMake(const std::string& formName, const std::string& target) {
+	sectionTitle(formName);
+
+	Form* form = interno.makeForm(formName, target);
+
+	std::cout << *form;
+	burro.signForm(*form);
+	burro.executeForm(*form);
+
+	delete form;
+}
 
 int	main() {
-	// sectionTitle("constructor");
-
 	try {
-		Bureaucrat	burro("burro", 42);
-		std::cout << burro;
-
-		Intern	burrino;
-
-		Form*	formino = burrino.makeForm("robotomy request", "Bender");
-		Form*	formone = burrino.makeForm("presidential pardon", "lrocca");
-
-		std::cout << *formino;
-		std::cout << *formone;
-
-		burro.signForm(*formino);
-		burro.executeForm(*formino);
-		burro.signForm(*formone);
-		burro.executeForm(*formone);
-
-		delete formino;
-		delete formone;
-
-		Form*	formaccio = burrino.makeForm("not a form", "you");
-		std::cout << "formaccio address: " << formaccio << std::endl;
+		testMake("robotomy request", "Bender");
+		testMake("presidential pardon", "lrocca");
+		testMake("shrubbery creation", "home");
+		testMake("not a form", "you");
 	}
 	catch (std::exception& e) {
 		std::cout << "Error: " << e.what() << std::endl;
 	}
-
-	// TODO
-	// make Shrubbery
 }
