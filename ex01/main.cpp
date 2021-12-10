bd495f5c8890eab307fc0bde6e99c7b31298972d
Leonardo Rocca <lrocca@student.42roma.it>
2021-12-10 18:25:10 +0100

    ex01: tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,38 +6,54 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 17:35:33 by lrocca            #+#    #+#             */
-/*   Updated: 2021/12/04 03:09:47 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/12/10 18:24:31 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 
-// void	sectionTitle(const std::string& title) {
-// 	std::cout	<< std::endl
-// 				<< "*** " << title << " ***"
-// 				<< std::endl << std::endl;
-// }
+void	sectionTitle(const std::string& title) {
+	std::cout	<< std::endl
+				<< "*** " << title << " ***"
+				<< std::endl << std::endl;
+}
+
+void	printError(std::exception& e) {
+	std::cout << "Error: " << e.what() << std::endl;
+}
 
 int	main() {
-	// sectionTitle("constructor");
+	Bureaucrat	burro("burro", 42);
+	std::cout << burro;
+
+	sectionTitle("constructor");
+	try {
+		Form	formaccio("formaccio", 1000, 1);
+	}
+	catch (std::exception& e) {
+		printError(e);
+	}
+	try {
+		Form	formaccio("formaccio", 42, 0);
+	}
+	catch (std::exception& e) {
+		printError(e);
+	}
 
 	try {
-		Bureaucrat	burro("burro", 42);
-		std::cout << burro;
-
 		Form	formino("formino", 42, 42);
 		Form	formone("formone", 1, 1);
 
-		std::cout << formino;
-		std::cout << formone;
+		std::cout << formino << formone;
 
+		sectionTitle("sign twice");
 		burro.signForm(formino);
+		burro.signForm(formino);
+		sectionTitle("grade too low");
 		burro.signForm(formone);
-
-		Form	formetto("formetto", 1000, 1);
 	}
 	catch (std::exception& e) {
-		std::cout << "Error: " << e.what() << std::endl;
+		printError(e);
 	}
 }
